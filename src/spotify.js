25440d0e01720ea2ecb0cedd4dec4dfc466e7bfd
morganhah <morganhah99@gmail.com>
2022-10-31 14:14:23 -0400

    added spotify authentication functionality to the login button using spotify's authorization link as an endpoint

--- src/spotify.js
+++ src/spotify.js
@@ -1,8 +1,20 @@
-export const authEndPoint = "https://accounts.spotify.com/authorize";
-
-const redirectUri = "http://locahost:3000/";
-
-
 //1. Click Login button
 //2. call spotify's authroization api to authroize user
 //3. Redirect user back to home page once authorizsed by spotify
+
+export const authEndPoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "39de5719de1a429889c2515fe939fc43";
+
+//scopes are permissions for users set by spotify. (current user doesn't have permission to remove songs but they have permission to read)
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&showdialog=true`;
