35bab813d7b5ef409e92dffbd4508e9909ea5037
morganhah <morganhah99@gmail.com>
2022-11-02 12:50:24 -0400

    finished set up for footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,32 +8,37 @@ import RepeatIcon from '@mui/icons-material/Repeat';
 import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
 import VolumeDownIcon from '@mui/icons-material/VolumeDown';
 import { Grid, Slider } from "@mui/material";
-
 function Footer() {
   return (
     <div className='footer'>
         <div className="footer__left">
-          <img src="" alt="" />
+          <img className="footer__albumLogo" src="" alt="album cover logo" />
+          <div className='footer__songInfo'>
+            <h4>Hello</h4>
+            <p>Eminem</p>
+          </div>
         </div>
 
         <div className='footer__center'>
-          <ShuffleIcon classname="footer__green" />
-          <SkipPreviousIcon classname="footer__icon" />
-          <PlayCircleFilledWhiteOutlinedIcon classname="footer__icon" />
+          <ShuffleIcon className="footer__green" />
+          <SkipPreviousIcon className="footer__icon" />
+          <PlayCircleFilledWhiteOutlinedIcon className="footer__icon" /> 
           <SkipNextIcon className='footer__icon' />
           <RepeatIcon className='footer__green' />
         </div>
-
+      
+      
+      
         <div className="footer__right">
         <Grid container spacing={2}>
-          <Grid item>
+          <Grid item>                
             <PlaylistPlayIcon />
           </Grid>
           <Grid item>
             <VolumeDownIcon />
           </Grid>
           <Grid item xs>
-            <Slider aria-labelledby="continuous-slider" />
+            <Slider />
           </Grid>
         </Grid>
       </div>
