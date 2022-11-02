fc7c4a28588edbc50298ac33c5aa8f58ca07b9e2
morganhah <morganhah99@gmail.com>
2022-11-02 13:34:09 -0400

    formatting

--- src/Body.js
+++ src/Body.js
@@ -5,9 +5,21 @@ import Header from "./Header"
 function Body({ spotify }) {
   return (
     <div className='body'>
-        <Header spotify={spotify}/>
+
+        <Header spotify={spotify}/> 
+
+        <div className='body__info'>
+
+          <img src="" alt="photo" />
+
+          <div className='body__infoText'>
+            <strong>PLAYLIST</strong>
+            <h2>Discover Weekly</h2>
+            <p>Description</p>
+          </div>
+        </div>
     </div>
   )
 }
-  
+
 export default Body
