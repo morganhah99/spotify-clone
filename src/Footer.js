b741ca9a26c63df1b12f9fde26f9b342f7ea9bce
Morgan Hah <mjh58@njit.edu>
2022-11-01 20:03:18 -0400

    set up player controls icons

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import "./Footer.css"
+import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
+
 
 function Footer() {
   return (
@@ -9,8 +15,11 @@ function Footer() {
         </div>
 
         <div className='footer__center'>
-          <p> Player controls</p>
-
+          <ShuffleIcon classname="footer__green" />
+          <SkipPreviousIcon classname="footer__icon" />
+          <PlayCircleFilledWhiteOutlinedIcon classname="footer__icon" />
+          <SkipNextIcon className='footer__icon' />
+          <RepeatIcon className='footer__green' />
         </div>
 
         <div className='footer__right'>
