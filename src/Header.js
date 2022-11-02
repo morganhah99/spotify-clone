7afe5024f5630f70b63af81459a0dd3821cb3678
morganhah <morganhah99@gmail.com>
2022-11-02 13:20:22 -0400

    pulled in user icon, name using spotify api

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,11 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import { Avatar } from "@mui/material";
+import { useDataLayerValue } from './DataLayer';
 
 function Header() {
+  const [{ user }, dispatch] = useDataLayerValue(); 
+
   return (
     <div className='header'>
       <div className='header__left'>
@@ -15,8 +18,8 @@ function Header() {
       </div>
 
       <div className='header__right'>
-        <Avatar src="" alt="profile picture" />
-        <h4>Morgan Hah</h4>
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
       </div>
 
     </div>
